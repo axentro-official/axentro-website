0a1d3202e8e0b384458f1abde6f0ea6253324b1a
AXENTRO <axentroofficial@gmail.com>
2026-09-11 15:43:39 +0300

    Fix service worker registration path

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,7 +106,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // 7. PWA Service Worker Registration
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js')
+      navigator.serviceWorker.register('/service-worker.js')
         .then(registration => {
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
         })
